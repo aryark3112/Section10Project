ccc0a760b09875a4e6cc76f30ad374609a254c54
aryark <aryark3112@gmail.com>
2023-07-31 08:56:29 +0530

    first commit

---
+++ TeacherWithSubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject_4
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
